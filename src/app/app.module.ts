fb9f9fd9cc16249e1695020e198f883a2ca3a288
iamvcm848 <valenmascarenhas14@gmail.com>
2021-11-16 14:47:05 +0530

    appointment calender creation and reactive form added along with a appointment calender service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { MyCalenderComponent } from './my-calender/my-calender.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BookAppointmentComponent } from './book-appointment/book-appointment.component';
 import { CreateNewCalenderComponent } from './create-new-calender/create-new-calender.component';
 import { LoginComponent } from './login/login.component';
@@ -18,6 +18,7 @@ import { RegisterComponent } from './register/register.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { UserService } from './user.service';
+import { CalenderlistComponent } from './calenderlist/calenderlist.component';
 
 @NgModule({
   declarations: [
@@ -30,12 +31,14 @@ import { UserService } from './user.service';
     BookAppointmentComponent,
     CreateNewCalenderComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    CalenderlistComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
---
+++ src/app/calenderlist/calenderlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calenderlist',
+  templateUrl: './calenderlist.component.html',
+  styleUrls: ['./calenderlist.component.css']
+})
+export class CalenderlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
