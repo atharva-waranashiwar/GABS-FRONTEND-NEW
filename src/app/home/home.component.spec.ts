60269a88f8f0cf284dfa0722f1d42ba601204ad4
Atharva Waranashiwar <atharva_waranashiwar@persistent.com>
2021-11-18 16:47:14 +0530

    Test cases added for app, header, navigation-bar, home components

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,16 @@
+import { LoginComponent } from './../login/login.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', () => {
+
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ,LoginComponent]
     })
     .compileComponents();
   });
@@ -17,9 +19,14 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should print welcome {{username}}', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h4').textContent).toContain(component.username);
   });
 });
